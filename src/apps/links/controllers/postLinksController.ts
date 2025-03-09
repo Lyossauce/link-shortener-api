e50ea85650a0e8c56348271bc6cc4e7cf247e65d
JMercierC <julien.mercier-chauve@hotmail.fr>
2025-03-09 15:06:58 +0100

    [stash] webpack error

--- src/apps/links/controllers/postLinksController.ts
+++ src/apps/links/controllers/postLinksController.ts
@@ -1,7 +1,12 @@
 import { APIGatewayProxyEvent } from 'aws-lambda';
+import { setRedisKey } from '../../services/redis/redis-service';
+import { generateNanoId } from '../../services/nanoid/nanoid-service';
 
 export const postLinksController = async (request: APIGatewayProxyEvent) => {
+    console.log('postLinksController', JSON.parse(request.body as string).url);
     // ADD VALIDATOR
     // ADD NANOID SERVICE
+    //const id = generateNanoId();
     // ADD REDIS SERVICE
+    await setRedisKey('id', JSON.parse(request.body as string).url);
 }
