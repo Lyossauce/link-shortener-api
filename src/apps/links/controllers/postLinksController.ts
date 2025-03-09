57381c7f2d441574ff72329876de349f83c4762a
JMercierC <julien.mercier-chauve@hotmail.fr>
2025-03-09 15:25:46 +0100

    [impl] Add redis service and finish endpoint

--- src/apps/links/controllers/postLinksController.ts
+++ src/apps/links/controllers/postLinksController.ts
@@ -1,7 +1,29 @@
 import { APIGatewayProxyEvent } from 'aws-lambda';
+import { setRedisKey } from '../../services/redis/redis-service';
+import { generateNanoId } from '../../services/nanoid/nanoid-service';
 
+// ADD ERROR HANDLING
+// ADD HOSTNAME HANDLING AS ENVIRONMENT VARIABLE
+
+
+/**
+ * Handles the creation of a new shortened link.
+ * 
+ * @param {APIGatewayProxyEvent} request - The incoming request object from API Gateway.
+ * @returns {Promise<{ statusCode: number, body: string }>} - The response object containing the status code and the body with the shortened URL.
+ * 
+ * @async
+ */
 export const postLinksController = async (request: APIGatewayProxyEvent) => {
     // ADD VALIDATOR
-    // ADD NANOID SERVICE
-    // ADD REDIS SERVICE
+    const url = JSON.parse(request.body as string).url
+    
+    const id = generateNanoId();
+    
+    await setRedisKey(id, url);
+
+    return {
+        statusCode: 201,
+        body: JSON.stringify({ url: `http:localhost:3000/local/${id}` })
+    }
 }
