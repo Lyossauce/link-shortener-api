0b131d3f5b227f18b77ea524ec75fda7fc4ce37f
JMercierC <julien.mercier-chauve@hotmail.fr>
2025-03-09 15:34:35 +0100

    [impl] Add redirect endpoint structure

---
+++ src/apps/links/controllers/redirectLinkController.ts
@@ -0,0 +1,6 @@
+import { APIGatewayProxyEvent } from 'aws-lambda';
+
+export const redirectLinkController = async (request: APIGatewayProxyEvent) => {
+    // ADD VALIDATOR
+    // ADD REDIS GET KEY
+}
